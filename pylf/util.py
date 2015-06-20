8e0f9d9929a4df8a482d825d504c75282254d074
Marc Schmitzer <marc@marc-schmitzer.de>
2015-06-20 11:57:00 +0200

    util: Add rel_path request method.

--- pylf/util.py
+++ pylf/util.py
@@ -1,5 +1,7 @@
 """Utility functions."""
 
+from urllib.parse import urlparse
+
 from jinja2 import Markup
 
 
@@ -24,11 +26,29 @@ def fmt_size(size):
     return Markup("{:g}&nbsp;{:s}".format(round(size, 2), sfx))
 
 
+def rel_path(request, abspath):
+    """Return a path relative to the current request location for abspath.
+    """
+    app_path = urlparse(request.application_url).path
+    req_path = request.path[len(app_path):].rstrip("/").split("/")
+    dst_path = abspath.split("/")
+
+    # get rid of a common prefix
+    while req_path and dst_path and req_path[0] == dst_path[0]:
+        req_path.pop(0)
+        dst_path.pop(0)
+
+    res_path = "/".join([".."] * len(req_path) + dst_path)
+    return res_path
+
+
 def includeme(config):
     """Setup function.
 
     Registers:
       * `fmt_size` as a jinja filter
+      * `rel_path` as a request method
     """
+    config.add_request_method(rel_path)
     j2env = config.get_jinja2_environment()
     j2env.filters["fmt_size"] = fmt_size
